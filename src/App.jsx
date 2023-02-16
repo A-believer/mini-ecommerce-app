518754cc452c107808f63cf3deebda17657b77c0
davedev <davidabolade29@gmail.com>
2023-02-16 16:24:37 +0100

    initial project setup complete

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import {Auth, Layout} from './components'
+function App() {
+ 
+
+  return (
+    <div className="App">
+      <Auth />
+      {/* <Layout/> */}
+    </div>
+  )
+}
+
+export default App
